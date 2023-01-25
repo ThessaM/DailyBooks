286b0c46deb75a864dbee75353f8f56d1e4ce806
Febrico Jonata <riko9647@gmail.com>
2023-01-25 16:57:39 +0700

    update project

--- project_mcc_lec/lib/page/loginpage.dart
+++ project_mcc_lec/lib/page/loginpage.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:project_mcc_lec/class/auth_service.dart';
 import 'package:project_mcc_lec/class/route.dart';
 import 'package:project_mcc_lec/page/homepage.dart';
 
